894dabc2a526e99df0b02bc99af4f928aa0c68a6
TeamBaconn <nguyenlamtuong2001@gmail.com>
2026-02-21 14:15:18 +0700

    Auto Deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import sitemap from 'vite-plugin-sitemap'
+import { resolve } from 'path'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
@@ -12,6 +13,14 @@ export default defineConfig(({ mode }) => {
         hostname: env.VITE_SITE_URL, 
       }),
     ],
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          vi: resolve(__dirname, 'vi/index.html'),
+        },
+      },
+    },
     base: '/',
   }
 })
